858385569b0bb2857cd7e31e8d034ad17789246c
Dkodsy <dkods15@mail.ru>
2020-12-13 22:56:54 +0600

    Добавил хендлеры на кнопки инструкции

--- handlers/users/instructrions.py
+++ handlers/users/instructrions.py
@@ -1,10 +1,47 @@
-from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.types import Message, CallbackQuery
 
-from keyboards.inline.instruction_buttons import choice
+from keyboards.inline.callback_datas import instructions_callback
+from keyboards.inline.instruction_buttons import choice_kkt
 from loader import dp
 
 
 @dp.message_handler(text='Инструкции кассира')
 async def show_instructions_buttons(message: Message):
-    await message.answer('Выберите необходимую модель ККТ', reply_markup=choice)
+    await message.answer('Выберите необходимую модель ККТ', reply_markup=choice_kkt)
 
+
+@dp.callback_query_handler(instructions_callback.filter(choice_kkt='atol_with_buttons'))
+async def inst_atol_with_buttons(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    await call.message.edit_text(text='TEXT')
+
+
+@dp.callback_query_handler(instructions_callback.filter(choice_kkt='atol_sigma'))
+async def inst_atol_with_buttons(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    await call.message.edit_text(text='TEXT')
+
+
+@dp.callback_query_handler(instructions_callback.filter(choice_kkt='evotor'))
+async def inst_atol_with_buttons(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    await call.message.edit_text(text='TEXT')
+
+
+@dp.callback_query_handler(instructions_callback.filter(choice_kkt='shtrix_elves'))
+async def inst_atol_with_buttons(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    await call.message.edit_text(text='TEXT')
+
+
+@dp.callback_query_handler(instructions_callback.filter(choice_kkt='mts'))
+async def inst_atol_with_buttons(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    await call.message.edit_text(text='TEXT')
+
+
+@dp.callback_query_handler(instructions_callback.filter(choice_kkt='mercury'))
+async def inst_atol_with_buttons(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    await call.message.edit_text(text='TEXT')
+# TODO нужно красиво оформить ответ в виде статьи
