9b9d1fe85415aa6e9d4ccdd869dcc3b179b643b2
Dkodsy <dkods15@mail.ru>
2020-12-13 22:56:05 +0600

    поменял название переменной

--- keyboards/inline/instruction_buttons.py
+++ keyboards/inline/instruction_buttons.py
@@ -2,27 +2,27 @@ from aiogram.types import InlineKeyboardMarkup
 
 from keyboards.inline.callback_datas import instructions_callback
 
-choice = InlineKeyboardMarkup(row_width=2)
+choice_kkt = InlineKeyboardMarkup(row_width=2)
 
 atol_with_buttons = InlineKeyboardMarkup(text='ККТ "АТОЛ(кнопочные)"',
                                          callback_data=instructions_callback.new(choice_kkt='atol_with_buttons'))
-choice.insert(atol_with_buttons)
+choice_kkt.insert(atol_with_buttons)
 
 atol_sigma = InlineKeyboardMarkup(text='💰 "АТОЛ Sigma"',
                                   callback_data=instructions_callback.new(choice_kkt='atol_sigma'))
-choice.insert(atol_sigma)
+choice_kkt.insert(atol_sigma)
 
 evotor = InlineKeyboardMarkup(text='ККТ "ЭВОТОР"',
                               callback_data=instructions_callback.new(choice_kkt='evotor'))
-choice.insert(evotor)
+choice_kkt.insert(evotor)
 
 shtrix_elves = InlineKeyboardMarkup(text='ККТ "ШТРИХ Элвес МФ"',
                                     callback_data=instructions_callback.new(choice_kkt='shtrix_elves'))
-choice.insert(shtrix_elves)
+choice_kkt.insert(shtrix_elves)
 
 mts = InlineKeyboardMarkup(text='ККТ "МТС"',
                            callback_data=instructions_callback.new(choice_kkt='mts'))
-choice.insert(mts)
+choice_kkt.insert(mts)
 
 mercury = InlineKeyboardMarkup(text='ККТ "Меркурий"', callback_data=instructions_callback.new(choice_kkt='mercury'))
-choice.insert(mercury)
+choice_kkt.insert(mercury)
