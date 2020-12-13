f224c125b50baa4ebb5d90614e0fcb73a77fd6bf
Dkodsy <dkods15@mail.ru>
2020-12-13 16:52:32 +0600

    Создание главного меню(еще будет допиливаться)

---
+++ keyboards/default/menu.py
@@ -0,0 +1,14 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='Инструкции кассира'),
+            KeyboardButton(text='Ошибки ККТ'),
+        ],
+        [
+            KeyboardButton(text='FAQ Часто задаваемые вопросы')
+        ],
+    ],
+    resize_keyboard=True
+)
