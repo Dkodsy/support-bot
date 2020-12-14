00b6be314456126d146b3965b20ac24d147fb789
Dkodsy <dkods15@mail.ru>
2020-12-14 18:57:36 +0600

    Изменил логику выбора кнопок ошибок

--- handlers/users/errors_kkt.py
+++ handlers/users/errors_kkt.py
@@ -1,10 +1,28 @@
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
-from keyboards.inline.error_buttons import choice_kkt_err
-
+from keyboards.inline.callback_datas import errors_callback
+from keyboards.inline.error_buttons import error_type, choice_model_kkt
 from loader import dp
 
 
 @dp.message_handler(text='Ошибки ККТ')
 async def show_instructions_buttons(message: Message):
-    await message.answer('Выберите необходимую модель ККТ', reply_markup=choice_kkt_err)
+    await message.answer('Выберите (не знаю что сюда дописать =) )', reply_markup=error_type)
+
+
+@dp.callback_query_handler(errors_callback.filter(choice_kkt_err='model'))
+async def inst_atol_with_buttons(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    await call.message.edit_text(text='Выберите необходимую модель ККТ', reply_markup=choice_model_kkt)
+
+
+@dp.callback_query_handler(errors_callback.filter(choice_kkt_err='popular'))
+async def inst_atol_with_buttons(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    await call.message.edit_text(text='TEXT')
+
+
+@dp.callback_query_handler(text='back')
+async def back(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    await call.message.answer('Ты нажал "НАЗАД"')
--- keyboards/inline/error_buttons.py
+++ keyboards/inline/error_buttons.py
@@ -2,13 +2,21 @@ from aiogram.types import InlineKeyboardMarkup
 
 from keyboards.inline.callback_datas import errors_callback
 
-choice_kkt_err = InlineKeyboardMarkup()
+error_type = InlineKeyboardMarkup()  # клавиатура для выбора (распространненых ошибок или по компаниям ККТ)
+common_error = InlineKeyboardMarkup(text='Популярные ошибки',
+                                    callback_data=errors_callback.new(choice_kkt_err='popular'))
+error_type.insert(common_error)
+
+model_kkt = InlineKeyboardMarkup(text='Выбор по модели ККТ', callback_data=errors_callback.new(choice_kkt_err='model'))
+error_type.insert(model_kkt)
+
+choice_model_kkt = InlineKeyboardMarkup()  # клавиатура для выбора компании ККТ
 
 atol = InlineKeyboardMarkup(text='АТОЛ', callback_data=errors_callback.new(choice_kkt_err='atol'))
-choice_kkt_err.insert(atol)
+choice_model_kkt.insert(atol)
 
 evotor = InlineKeyboardMarkup(text='ЭВОТОР', callback_data=errors_callback.new(choice_kkt_err='evotor'))
-choice_kkt_err.insert(evotor)
+choice_model_kkt.insert(evotor)
 
 mts = InlineKeyboardMarkup(text='МТС', callback_data=errors_callback.new(choice_kkt_err='mts'))
-choice_kkt_err.insert(mts)
+choice_model_kkt.insert(mts)
