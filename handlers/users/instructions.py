d8e1f04e705532a8c9d02151d73ac8e727f34466
Dkodsy <dkods15@mail.ru>
2020-12-14 17:04:42 +0600

    Поменял название переменных

--- handlers/users/instructions.py
+++ handlers/users/instructions.py
@@ -10,37 +10,37 @@ async def show_instructions_buttons(message: Message):
     await message.answer('Выберите необходимую модель ККТ', reply_markup=choice_kkt)
 
 
-@dp.callback_query_handler(instructions_callback.filter(choice_kkt='atol_with_buttons'))
+@dp.callback_query_handler(instructions_callback.filter(choice_kkt_instr='atol_with_buttons'))
 async def inst_atol_with_buttons(call: CallbackQuery):
     await call.answer(cache_time=60)
     await call.message.edit_text(text='TEXT')
 
 
-@dp.callback_query_handler(instructions_callback.filter(choice_kkt='atol_sigma'))
+@dp.callback_query_handler(instructions_callback.filter(choice_kkt_instr='atol_sigma'))
 async def inst_atol_with_buttons(call: CallbackQuery):
     await call.answer(cache_time=60)
     await call.message.edit_text(text='TEXT')
 
 
-@dp.callback_query_handler(instructions_callback.filter(choice_kkt='evotor'))
+@dp.callback_query_handler(instructions_callback.filter(choice_kkt_instr='evotor'))
 async def inst_atol_with_buttons(call: CallbackQuery):
     await call.answer(cache_time=60)
     await call.message.edit_text(text='TEXT')
 
 
-@dp.callback_query_handler(instructions_callback.filter(choice_kkt='shtrix_elves'))
+@dp.callback_query_handler(instructions_callback.filter(choice_kkt_instr='shtrix_elves'))
 async def inst_atol_with_buttons(call: CallbackQuery):
     await call.answer(cache_time=60)
     await call.message.edit_text(text='TEXT')
 
 
-@dp.callback_query_handler(instructions_callback.filter(choice_kkt='mts'))
+@dp.callback_query_handler(instructions_callback.filter(choice_kkt_instr='mts'))
 async def inst_atol_with_buttons(call: CallbackQuery):
     await call.answer(cache_time=60)
     await call.message.edit_text(text='TEXT')
 
 
-@dp.callback_query_handler(instructions_callback.filter(choice_kkt='mercury'))
+@dp.callback_query_handler(instructions_callback.filter(choice_kkt_instr='mercury'))
 async def inst_atol_with_buttons(call: CallbackQuery):
     await call.answer(cache_time=60)
     await call.message.edit_text(text='TEXT')
--- keyboards/inline/instruction_buttons.py
+++ keyboards/inline/instruction_buttons.py
@@ -5,24 +5,24 @@ from keyboards.inline.callback_datas import instructions_callback
 choice_kkt = InlineKeyboardMarkup(row_width=2)
 
 atol_with_buttons = InlineKeyboardMarkup(text='ККТ "АТОЛ(кнопочные)"',
-                                         callback_data=instructions_callback.new(choice_kkt='atol_with_buttons'))
+                                         callback_data=instructions_callback.new(choice_kkt_instr='atol_with_buttons'))
 choice_kkt.insert(atol_with_buttons)
 
 atol_sigma = InlineKeyboardMarkup(text='💰 "АТОЛ Sigma"',
-                                  callback_data=instructions_callback.new(choice_kkt='atol_sigma'))
+                                  callback_data=instructions_callback.new(choice_kkt_instr='atol_sigma'))
 choice_kkt.insert(atol_sigma)
 
 evotor = InlineKeyboardMarkup(text='ККТ "ЭВОТОР"',
-                              callback_data=instructions_callback.new(choice_kkt='evotor'))
+                              callback_data=instructions_callback.new(choice_kkt_instr='evotor'))
 choice_kkt.insert(evotor)
 
 shtrix_elves = InlineKeyboardMarkup(text='ККТ "ШТРИХ Элвес МФ"',
-                                    callback_data=instructions_callback.new(choice_kkt='shtrix_elves'))
+                                    callback_data=instructions_callback.new(choice_kkt_instr='shtrix_elves'))
 choice_kkt.insert(shtrix_elves)
 
 mts = InlineKeyboardMarkup(text='ККТ "МТС"',
-                           callback_data=instructions_callback.new(choice_kkt='mts'))
+                           callback_data=instructions_callback.new(choice_kkt_instr='mts'))
 choice_kkt.insert(mts)
 
-mercury = InlineKeyboardMarkup(text='ККТ "Меркурий"', callback_data=instructions_callback.new(choice_kkt='mercury'))
+mercury = InlineKeyboardMarkup(text='ККТ "Меркурий"', callback_data=instructions_callback.new(choice_kkt_instr='mercury'))
 choice_kkt.insert(mercury)
