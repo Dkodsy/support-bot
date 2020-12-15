0e06531e28f750379b2f9f96cae9553c8b5e5ffb
Dkodsy <dkods15@mail.ru>
2020-12-15 16:30:35 +0600

    работа на error кнопками, добавил кнопку 'назад'

--- handlers/users/errors_kkt.py
+++ handlers/users/errors_kkt.py
@@ -7,13 +7,7 @@ from loader import dp
 
 @dp.message_handler(text='Ошибки ККТ')
 async def show_instructions_buttons(message: Message):
-    await message.answer('Выберите (не знаю что сюда дописать =) )', reply_markup=error_type)
-
-
-@dp.callback_query_handler(errors_callback.filter(choice_kkt_err='model'))
-async def inst_atol_with_buttons(call: CallbackQuery):
-    await call.answer(cache_time=60)
-    await call.message.edit_text(text='Выберите необходимую модель ККТ', reply_markup=choice_model_kkt)
+    await message.answer('Выберите ошибку по номеру или по модели ККТ', reply_markup=error_type)
 
 
 @dp.callback_query_handler(errors_callback.filter(choice_kkt_err='popular'))
@@ -22,7 +16,17 @@ async def inst_atol_with_buttons(call: CallbackQuery):
     await call.message.edit_text(text='TEXT')
 
 
-@dp.callback_query_handler(text='back')
+@dp.callback_query_handler(errors_callback.filter(choice_kkt_err='model'))
+async def inst_atol_with_buttons(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    await call.message.edit_text(text='Выберите необходимую модель ККТ', reply_markup=choice_model_kkt)
+
+
+@dp.callback_query_handler(text='back_to_type_error')
 async def back(call: CallbackQuery):
     await call.answer(cache_time=60)
-    await call.message.answer('Ты нажал "НАЗАД"')
+    await call.message.delete()
+    await call.message.answer('Выберите ошибку по номеру или по модели ККТ',
+                              reply_markup=error_type)  # здесь это реализовано через тупой костыль "удалить
+    # сообщение и прислать новое
+    # TODO в будущем нужно будет переписать эту кнопку(скорее всего кнопкИ)"
--- keyboards/inline/error_buttons.py
+++ keyboards/inline/error_buttons.py
@@ -1,22 +1,36 @@
-from aiogram.types import InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from keyboards.inline.callback_datas import errors_callback
 
-error_type = InlineKeyboardMarkup()  # клавиатура для выбора (распространненых ошибок или по компаниям ККТ)
-common_error = InlineKeyboardMarkup(text='Популярные ошибки',
-                                    callback_data=errors_callback.new(choice_kkt_err='popular'))
-error_type.insert(common_error)
+back = InlineKeyboardButton(text='НАЗАД', callback_data='back_to_type_error')  # кнопка "назад"
 
-model_kkt = InlineKeyboardMarkup(text='Выбор по модели ККТ', callback_data=errors_callback.new(choice_kkt_err='model'))
-error_type.insert(model_kkt)
+# клавиатура для выбора (распространненых ошибок или по компаниям ККТ)
+error_type = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='Найти ошибку по номеру',
+                                 callback_data=errors_callback.new(choice_kkt_err='popular'))
+        ],
+        [
+            InlineKeyboardButton(text='Выбор ошибки по модели ККТ',
+                                 callback_data=errors_callback.new(choice_kkt_err='model'))
+        ]
+    ]
+)
 
-choice_model_kkt = InlineKeyboardMarkup()  # клавиатура для выбора компании ККТ
+# клавиатура для выбора модели ККТ
+choice_model_kkt = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='АТОЛ', callback_data=errors_callback.new(choice_kkt_err='atol')),
+            InlineKeyboardButton(text='ЭВОТОР', callback_data=errors_callback.new(choice_kkt_err='evotor')),
+            InlineKeyboardButton(text='МТС', callback_data=errors_callback.new(choice_kkt_err='mts')),
 
-atol = InlineKeyboardMarkup(text='АТОЛ', callback_data=errors_callback.new(choice_kkt_err='atol'))
-choice_model_kkt.insert(atol)
+        ],
+        [
+            back
+        ]
+    ]
+)
 
-evotor = InlineKeyboardMarkup(text='ЭВОТОР', callback_data=errors_callback.new(choice_kkt_err='evotor'))
-choice_model_kkt.insert(evotor)
-
-mts = InlineKeyboardMarkup(text='МТС', callback_data=errors_callback.new(choice_kkt_err='mts'))
-choice_model_kkt.insert(mts)
+# список распространненых ошибок
