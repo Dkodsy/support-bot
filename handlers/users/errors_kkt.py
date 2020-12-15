500efa90c90f016231551216ad1eb30b976ce5d8
Dkodsy <dkods15@mail.ru>
2020-12-15 17:54:48 +0600

    Работа над кнопками

--- handlers/users/errors_kkt.py
+++ handlers/users/errors_kkt.py
@@ -1,19 +1,15 @@
 from aiogram.types import Message, CallbackQuery
 
 from keyboards.inline.callback_datas import errors_callback
-from keyboards.inline.error_buttons import error_type, choice_model_kkt
+from keyboards.inline.error_buttons import choice_model_kkt, popular_errors
 from loader import dp
 
 
 @dp.message_handler(text='Ошибки ККТ')
 async def show_instructions_buttons(message: Message):
-    await message.answer('Выберите ошибку по номеру или по модели ККТ', reply_markup=error_type)
-
-
-@dp.callback_query_handler(errors_callback.filter(choice_kkt_err='popular'))
-async def inst_atol_with_buttons(call: CallbackQuery):
-    await call.answer(cache_time=60)
-    await call.message.edit_text(text='TEXT')
+    await message.answer(text='Выберите номер ошибки\n'
+                              'Если вы не нашли ошибку, то выберите по модели ККТ\n'
+                              'Скорее всего вы найдете ошибку там\n', reply_markup=popular_errors)
 
 
 @dp.callback_query_handler(errors_callback.filter(choice_kkt_err='model'))
@@ -22,11 +18,11 @@ async def inst_atol_with_buttons(call: CallbackQuery):
     await call.message.edit_text(text='Выберите необходимую модель ККТ', reply_markup=choice_model_kkt)
 
 
-@dp.callback_query_handler(text='back_to_type_error')
+@dp.callback_query_handler(text='back_error')
 async def back(call: CallbackQuery):
     await call.answer(cache_time=60)
     await call.message.delete()
     await call.message.answer('Выберите ошибку по номеру или по модели ККТ',
-                              reply_markup=error_type)  # здесь это реализовано через тупой костыль "удалить
+                              reply_markup=popular_errors)  # здесь это реализовано через тупой костыль "удалить
     # сообщение и прислать новое
     # TODO в будущем нужно будет переписать эту кнопку(скорее всего кнопкИ)"
--- keyboards/inline/error_buttons.py
+++ keyboards/inline/error_buttons.py
@@ -2,21 +2,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from keyboards.inline.callback_datas import errors_callback
 
-back = InlineKeyboardButton(text='НАЗАД', callback_data='back_to_type_error')  # кнопка "назад"
-
-# клавиатура для выбора (распространненых ошибок или по компаниям ККТ)
-error_type = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [
-            InlineKeyboardButton(text='Найти ошибку по номеру',
-                                 callback_data=errors_callback.new(choice_kkt_err='popular'))
-        ],
-        [
-            InlineKeyboardButton(text='Выбор ошибки по модели ККТ',
-                                 callback_data=errors_callback.new(choice_kkt_err='model'))
-        ]
-    ]
-)
+back = InlineKeyboardButton(text='НАЗАД', callback_data='back_error')  # кнопка "назад"
 
 # клавиатура для выбора модели ККТ
 choice_model_kkt = InlineKeyboardMarkup(
@@ -34,3 +20,30 @@ choice_model_kkt = InlineKeyboardMarkup(
 )
 
 # список распространненых ошибок
+popular_errors = InlineKeyboardMarkup(  # ToDo Заполнить номера ошибок корректно(прописать колбэк дату)
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='№211', callback_data=errors_callback.new(choice_kkt_err='atol')),
+            InlineKeyboardButton(text='№217', callback_data=errors_callback.new(choice_kkt_err='evotor')),
+            InlineKeyboardButton(text='№218', callback_data=errors_callback.new(choice_kkt_err='mts')),
+            InlineKeyboardButton(text='№231', callback_data=errors_callback.new(choice_kkt_err='atol')),
+            InlineKeyboardButton(text='№234', callback_data=errors_callback.new(choice_kkt_err='evotor')),
+            InlineKeyboardButton(text='№3807', callback_data=errors_callback.new(choice_kkt_err='atol')),
+
+        ],
+        [
+            InlineKeyboardButton(text='№235', callback_data=errors_callback.new(choice_kkt_err='atol')),
+            InlineKeyboardButton(text='№3933', callback_data=errors_callback.new(choice_kkt_err='atol')),
+            InlineKeyboardButton(text='№3924', callback_data=errors_callback.new(choice_kkt_err='atol')),
+            InlineKeyboardButton(text='№240', callback_data=errors_callback.new(choice_kkt_err='atol')),
+            InlineKeyboardButton(text='№210', callback_data=errors_callback.new(choice_kkt_err='atol')),
+            InlineKeyboardButton(text='№244', callback_data=errors_callback.new(choice_kkt_err='atol')),
+
+        ],
+        [
+            InlineKeyboardButton(text='Выбор ошибки по модели ККТ',
+                                 callback_data=errors_callback.new(choice_kkt_err='model'))
+        ],
+    ],
+    row_width=1
+)
