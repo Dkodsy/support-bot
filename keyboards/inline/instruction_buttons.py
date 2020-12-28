34dd499d303c7f0fb7d8856ab7daa15631565634
Dkodsy <dkods15@mail.ru>
2020-12-28 14:55:43 +0600

    сделал кнопки в столбик

--- keyboards/inline/instruction_buttons.py
+++ keyboards/inline/instruction_buttons.py
@@ -8,20 +8,26 @@ async def instructions_buttons():
         inline_keyboard=[
             [
                 InlineKeyboardButton(text='ККТ "АТОЛ(кнопочные)"',
-                                     callback_data=menu_cd.new(category='instructions', subcategory='atol_with_buttons')),
+                                     callback_data=menu_cd.new(category='instructions',
+                                                               subcategory='atol_with_buttons')),
+            ],
+            [
                 InlineKeyboardButton(text='💰 "АТОЛ Sigma"',
                                      callback_data=menu_cd.new(category='instructions', subcategory='atol_sigma')),
-
             ],
             [
                 InlineKeyboardButton(text='ККТ "ЭВОТОР"',
                                      callback_data=menu_cd.new(category='instructions', subcategory='evotor')),
+            ],
+            [
                 InlineKeyboardButton(text='ККТ "ШТРИХ Элвес МФ"',
                                      callback_data=menu_cd.new(category='instructions', subcategory='shtrix_elves')),
             ],
             [
                 InlineKeyboardButton(text='ККТ "МТС"',
                                      callback_data=menu_cd.new(category='instructions', subcategory='mts')),
+            ],
+            [
                 InlineKeyboardButton(text='ККТ "Меркурий"',
                                      callback_data=menu_cd.new(category='instructions', subcategory='mercury'))
             ]
